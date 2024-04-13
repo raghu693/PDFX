47fe7642cd8436c18863962173d94a14a340c611
raghu693 <sonia.nagar16@gmail.com>
2024-04-13 18:13:06 +0530

    first commit

---
+++ merge.js
@@ -0,0 +1,15 @@
+import PDFMerger from 'pdf-merger-js';
+
+let merger = new PDFMerger();
+
+let pdfmerger = async (p1, p2) => {
+  await merger.add(p1);  //merge all pages. parameter is the path to file and filename.
+  await merger.add(p2)
+
+  let ct = new Date().getTime()
+  await merger.save(`public/${ct}.pdf`);
+  return ct
+};
+
+
+export default pdfmerger
---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import multer from 'multer'
+const upload = multer({ dest: 'uploads/' })
+import pdfmerger from './merge.js'
+let __filename = fileURLToPath(import.meta.url)
+let __dirname = path.dirname(__filename)
+let app = express()
+let port = 3000
+
+app.use('/static', express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile('./index.html', { root: __dirname })
+})
+
+app.post('/merge', upload.array('pdfs', 2), async (req, res, next) => {
+    // res.send({data : req.files})
+    let ct = await pdfmerger(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
+    res.redirect(`http://localhost:3000/static/${ct}.pdf`)
+})
+
+app.listen(port, () => {
+    console.log('server at 3000')
+})
